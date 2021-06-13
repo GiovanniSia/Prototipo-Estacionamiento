739b3a763ea2169534dcced61b02ea853ff0993d
sebasx3601 <sebastianx3600@gmail.com>
2021-06-13 02:07:54 -0300

    Se borraron funciones que no se usaban
    QED esas funciones que no llegaron a ver el async await

--- Infracciones/cliente.js
+++ Infracciones/cliente.js
@@ -65,7 +65,7 @@ var bootstrap = function () {
         console.log(response)
 	//actualizarElHTMLConLasInfracciones(response)
         //agregartxt1(response)
-	buscarYEscribirLasInfracciones(response)
+	escribirLasInfraccionesEnElHtml(response)
     }
 
     requestInfracciones(numPatente)
@@ -87,82 +87,25 @@ var bootstrap = function () {
 	return response.descripcion;
     }
 
-    function agregartxt1(response) {
-        document.getElementById("infracciones").innerHTML = "";
-        for (let x = 0; x < response.length; x++) {
-	    obtenerTipoInfraccion(parseInt(response[x].tipoInfraccion))
-            direccionRegistrada = response[x].direccionRegistrada
-            existeAcarreo = response[x].existeAcarreo
-            fechaHoraActualizacion = response[x].fechaHoraActualizacion
-            fechaHoraRegistro = response[x].fechaHoraRegistro
-            id = response[x].id
-            montoAPagar = response[x].montoAPagar;
-            patente = response[x].patente;
-            
-console.log(tipoInfra);
-            var text = document.createTextNode("id: " + id + ", " + direccionRegistrada + ", fecha de actualizacion:" + fechaHoraActualizacion + ", fecha de registro: " + fechaHoraRegistro + ", monto a pagar: " + montoAPagar + ", tipo de infraccion: "+tipoInfra);
-		
-            document.getElementById("infracciones").appendChild(text);
-            var newt = document.createElement("br");
-            document.getElementById("infracciones").appendChild(newt); 
-        }
-    }
-// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-//	Tratando de hacerlo de forma secuencial
+//SEGUNDO INTENTO DE SECUENCIARLO
+	var escribirInfraccionesEnHtml = function (infracciones){
+		for (let x = 0; x < infracciones.length; x++) {
+			direccionRegistrada = infracciones[x].direccionRegistrada
+        	    	existeAcarreo = infracciones[x].existeAcarreo
+        	   	fechaHoraActualizacion = infracciones[x].fechaHoraActualizacion
+        	    	fechaHoraRegistro = infracciones[x].fechaHoraRegistro
+        	    	id = infracciones[x].id
+        	    	montoAPagar = infracciones[x].montoAPagar;
+       		     	patente = infracciones[x].patente;
+        	    	tipoInfraccion = infracciones[x].tipoInfraccionString
 
-	function actualizarElHTMLConLasInfracciones(response) {
-		document.getElementById("infracciones").innerHTML = "";
-        	for (let x = 0; x < response.length; x++) {
-			obtenerTipoDeInfraccion(response[x]).then(escribirEnElHTML(response[x]));
+         	   	var text = document.createTextNode("id: " + id + ", " + direccionRegistrada + ", fecha de actualizacion:" + fechaHoraActualizacion + ", fecha de registro: " + fechaHoraRegistro + ", monto a pagar: " + montoAPagar + ", tipo de infraccion: "+ tipoInfraccion);
+
+          	  	document.getElementById("infracciones").appendChild(text);
+          	  	var newt = document.createElement("br");
+         	   	document.getElementById("infracciones").appendChild(newt);
 		}
 	}
-
-	var obtenerTipoDeInfraccion = function (infraccion){
-		tipoInfraccion = obtenerTipoInfraccion(infraccion.tipoInfraccion)
-		console.log(tipoInfraccion + " " + tipoInfra + " llego hasta aca")
-		return tipoInfraccion
-	}
-
-	var escribirEnElHTML = function (infraccion){
-		direccionRegistrada = infraccion.direccionRegistrada
-            	existeAcarreo = infraccion.existeAcarreo
-           	fechaHoraActualizacion = infraccion.fechaHoraActualizacion
-            	fechaHoraRegistro = infraccion.fechaHoraRegistro
-            	id = infraccion.id
-            	montoAPagar = infraccion.montoAPagar;
-            	patente = infraccion.patente;
-            	tipoInfraccion = tipoInfra
-
-            	var text = document.createTextNode("id: " + id + ", " + direccionRegistrada + ", fecha de actualizacion:" + fechaHoraActualizacion + ", fecha de registro: " + fechaHoraRegistro + ", monto a pagar: " + montoAPagar + ", tipo de infraccion: "+tipoInfraccion);
-		
-            	document.getElementById("infracciones").appendChild(text);
-            	var newt = document.createElement("br");
-            	document.getElementById("infracciones").appendChild(newt);
-	}
-	
-
-// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-
-//SEGUNDO INTENTO DE SECUENCIARLO
-
-	var escribirInfraccionesEnHtml = function (infracciones){
-	for (let x = 0; x < infracciones.length; x++) {
-		direccionRegistrada = infracciones[x].direccionRegistrada
-            	existeAcarreo = infracciones[x].existeAcarreo
-           	fechaHoraActualizacion = infracciones[x].fechaHoraActualizacion
-            	fechaHoraRegistro = infracciones[x].fechaHoraRegistro
-            	id = infracciones[x].id
-            	montoAPagar = infracciones[x].montoAPagar;
-            	patente = infracciones[x].patente;
-            	tipoInfraccion = infracciones[x].tipoInfraccionString
-
-            	var text = document.createTextNode("id: " + id + ", " + direccionRegistrada + ", fecha de actualizacion:" + fechaHoraActualizacion + ", fecha de registro: " + fechaHoraRegistro + ", monto a pagar: " + montoAPagar + ", tipo de infraccion: "+ tipoInfraccion);
-
-            	document.getElementById("infracciones").appendChild(text);
-            	var newt = document.createElement("br");
-            	document.getElementById("infracciones").appendChild(newt);
-	}
-}
 
 	var buscarEInsertarLosTiposDeInfracciones = async function (infracciones){
 		for (let x = 0; x < infracciones.length; x++) {
@@ -173,7 +116,8 @@ console.log(tipoInfra);
 		return infracciones;
 	};
 
-	async function buscarYEscribirLasInfracciones(infracciones){
+	async function escribirLasInfraccionesEnElHtml(infracciones){
+		document.getElementById("infracciones").innerHTML = "";
 		const infraccionesActualizadas = await buscarEInsertarLosTiposDeInfracciones(infracciones)
 		await escribirInfraccionesEnHtml(infraccionesActualizadas)
 		console.log("Cumplio la segunda de secuencializado")
