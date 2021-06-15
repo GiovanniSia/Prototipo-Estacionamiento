61f4249679c7ab53d8f767dbbc33a593f26ca674
sebasx3601 <sebastianx3600@gmail.com>
2021-06-15 03:18:11 -0300

    El boton anda pero sigue mal posicionado
    a

--- Infracciones/mapCreation.js
+++ Infracciones/mapCreation.js
@@ -3,13 +3,13 @@
  */
 var map;
 var p;
-var createMap = function(nodeId, datosAcarreo) {
+var createMap = async function(nodeId, datosAcarreo) {
     // Ubicación del deposito
     var depositoLocation = [datosAcarreo.deposito.ubicacion.lat, datosAcarreo.deposito.ubicacion.lon];
 
     // Creación del componente mapa de Leaflet.
     if(map==null){
-        map = L.map(nodeId).setView(depositoLocation, 13);
+        map = await L.map(nodeId).setView(depositoLocation, 13);
 	// Agregamos los Layers de OpenStreetMap.
    	    var baseLayer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
@@ -23,14 +23,18 @@ var createMap = function(nodeId, datosAcarreo) {
     	// hack:
     	map.layersControl = layersControl;
 
+	mapid = $("#mapid");
+	const buttonBorrar = '<input type="submit" value="Cerrar mapa" id="Cerrar_mapa" name="Cerrar mapa"/>';
+	$('#mapid').append(buttonBorrar);
+	$("#Cerrar_mapa").click(function() {
+		mapid.css('opacity','0')
+    	});
+
     }else{
         console.log("PENE");
 	    p.remove();
     }
     
-
-    
-     
     p = L.marker(L.latLng(depositoLocation[0], depositoLocation[1])).bindPopup("Deposito del acarreo: " + datosAcarreo.deposito.nombre + ", direccion:" + datosAcarreo.deposito.direccion + ", horarios:" + datosAcarreo.deposito.horarios + ", telefono: " + datosAcarreo.deposito.telefono);
     p.addTo(map);	
 
--- Infracciones/mapa.js
+++ Infracciones/mapa.js
@@ -1,16 +1,8 @@
 
 async function mostrarEnElMapaDeposito(datosAcarreo){
 	//var ubicacionDeposito = [datosAcarreo.deposito.ubicacion.lat, datosAcarreo.deposito.ubicacion.lon];
-	var mapid = $("#mapid");
+	mapid = $("#mapid");
 	mapid.css('opacity','1')
 	console.log("DATOS ACARREO: ",datosAcarreo);
 	await createMap('mapid', datosAcarreo);
-
-	const buttonBorrar = '<input type="submit" value="Cerrar mapa" id="Cerrar_mapa" name="Cerrar mapa"/>';
-	$('#mapid').append(buttonBorrar);
-
-	$("#Cerrar_mapa").click(function() {
-		mapid = $("#mapid");
-		mapid.css('opacity','0')
-    	});
 }
